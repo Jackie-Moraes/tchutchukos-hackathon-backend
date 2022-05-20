c65945b6453c5af51344a5dcd3e6ffe9e5a53f5f
José Rodolpho <j.rodolpho29@hotmail.com>
2022-05-20 19:59:40 -0300

    feat: insert comments

--- routes/videosRouter.js
+++ routes/videosRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { getVideos, getSpecificVideo } from "../controllers/videosController.js"
+import { getVideos, getSpecificVideo, insertComment } from "../controllers/videosController.js"
 
 const videosRouter = Router()
 
 videosRouter.get("/videos", getVideos)
 videosRouter.get("/videos/:id", getSpecificVideo)
+videosRouter.post("/videos/:id", insertComment)
 
 export default videosRouter
